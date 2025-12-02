3677aa8c5fc088ab2a80d85c37c88e3afb71d863
Evgeny Levinsky <evgeny@ideal-code.com>
2025-12-02 12:49:48 -0600

    Add generic commerce action: initial implementation

---
+++ actions/commerce/generic/index.js
@@ -0,0 +1,22 @@
+export const main = (params) => {
+  let result
+
+  try {
+    // let's do something
+    result = {
+      success: true
+    }
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: {
+        error: e.message
+      }
+    }
+  }
+
+  return {
+    statusCode: 200,
+    body: result
+  }
+}
