62e8984ec013544d05d5a89cd44b0583e87e5ae3
Evgeny Levinsky <evgeny@ideal-code.com>
2025-12-02 14:07:01 -0600

    Add sample of onboarding script for connecting with backoffice

--- actions/commerce/generic/index.js
+++ actions/commerce/generic/index.js
@@ -1,9 +1,15 @@
+/**
+ * Extremely simple example of runtime action, which
+ * can be used to call external APIs, receiving events
+ * from Adobe Commerce or anything else.
+ */
+
 // eslint-disable-next-line no-unused-vars
 export const main = (params) => {
   let result
 
   try {
-    // let's do something
+    // @todo let's do something
     result = {
       success: true
     }
---
+++ scripts/onboarding/index.js
@@ -0,0 +1,7 @@
+/**
+ * The purpose of the onboarding script primarily is
+ * to create new entities in Adobe I/O Events
+ * or registering the app in the backoffice.
+ */
+
+console.log('You are onboarding!')
